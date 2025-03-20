851492fcad0ae8c889618e7a49ddfe77ad085f57
SIMONE QUEIROZ BATISTA <simone.qbatista@senacsp.edu.br>
2025-03-19 22:12:44 -0300

    inclusão de funcionalidades menu, botao, script.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,62 @@
-//selecionar os elemntos dos slides
+// selecionando elementos do menu Mobile
+const menuBtn = document.querySelector("#menu");
+const closeMenuBtn = document.querySelector("#close-menu");
+const menu = document.querySelector("#mobile-navbar");
+
+// Fechar o menu, apos clicar na opçao sobre
+//Feito por Walisson e Sena
+const about = document.querySelector('#mobile-navbar [href="#about"]');
+
+//selecionar os elementos dos slides
 const slides = document.querySelectorAll(".banner");
 //
 const dots = document.querySelectorAll(".dot");
 // variavel de controle
-let slideIndex = 0;
+let slideIndex = 1;
 
-function  showSlides() {
+function showSlides() {
+  //console.log(slides)
     for(let i=0; i< slides.length; i++){
-      slides[i].classList.remove("active")
-      dots[i].classList.remove("active")
+    slides[i].classList.remove("active")
+    dots[i].classList.remove("active")
     }
 
     slideIndex++; //avançando a variavel
     if (slideIndex > slides.length){
-      slideIndex = 1;
-    }
-
+        slideIndex = 1;
+   }
+      console.log(slideIndex)
+      console.log(slides[slideIndex -1])
     slides[slideIndex -1].classList.add("active")
     dots[slideIndex -1].classList.add("active")
 
     setTimeout(showSlides, 3000)
 }
 
-//Inicialização
+
+//Ativar menu-Mobile
+// arrow function
+// () => {
+// }
+// function default
+// function () {
+// }
+//menuBtn.addEventListener("click", (e) => {
+    //menu.classList.add("menu-active")
+//})
+//closeMenuBtn.addEventListener("click", (e) => {
+     // menu.classList.add("menu-active")
+//})
+[menuBtn, closeMenuBtn].forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    menu.classList.toggle("menu-active")
+  })
+})
+
+
+
+
+//Inicialização do slides 
 showSlides()
 
 
